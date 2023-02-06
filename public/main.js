c4ff7eee28c1534f355653c7d854010b2e83c48d
DerSmiley93 <philliphecker@hotmail.com>
2023-02-06 12:34:37 +0100

    Remaked player controler

--- public/main.js
+++ public/main.js
@@ -8,10 +8,11 @@ canvas.height = height;
 var ctx = canvas.getContext('2d');
 
 var engine = new Game(ctx,width,height);
-var player = new Player(engine,new Vector2D(0,Metric.km),new Vector2D(50,180 * Metric.cm));
-
-
+var worldGen = new WorldGenerator(engine,new Vector2D(0,0),new Vector2D(0,0),"worldGenerator")
+var player = new Player(engine,new Vector2D(0,Metric.m * 5),new Vector2D(50,180 * Metric.cm),"palyer");
+engine.addGameobject(worldGen);
 engine.addGameobject(player);
+console.log(engine.gameobjects)
 
 
 function tick(){
--- public/prototypes.js
+++ public/prototypes.js
@@ -12,7 +12,7 @@ class Game {
 
   updateAndDraw() {
     this.deltaTime = (this.frameEnd - this.frameStart) / 1000;
-    if(this.deltaTime < 0.001){
+    if (this.deltaTime < 0.001) {
       this.deltaTime = 0.001;
     }
     this.frameStart = Date.now();
@@ -24,11 +24,11 @@ class Game {
     }
 
     this.frameEnd = Date.now();
-
   }
 
   addGameobject(obj) {
     this.gameobjects.push(obj);
+    obj.onInit();
   }
 }
 
@@ -40,14 +40,15 @@ class Vector2D {
 }
 
 class Gameobject {
-  update = () => {};
-  draw = () => {};
-
-  constructor(engine, pos, size) {
+  constructor(engine, pos, size, name) {
     this.engine = engine;
     this.pos = pos;
     this.size = size;
+    this.name = name;
   }
+  onInit() {}
+  update() {}
+  draw() {}
 }
 
 class Metric {
@@ -60,31 +61,114 @@ class Metric {
 }
 
 class Player extends Gameobject {
-  mass = 1;
+  //mass in kg
+  mass = 80;
+  //vel in m/s
   vel = new Vector2D(0, 0);
-  acc = new Vector2D(0, 0);
 
-  update = () => {
-    if(this.pos.y <= 0){
-      this.vel.y = 0;
-      this.pos.y = 0;
+  jumpForce = 80;
+  jumpTime = 8000;
+  isOnGround = false;
+
+  dDown = false;
+  aDown = false;
+
+  onInit() {
+    //handle playerInput
+
+    document.onkeydown = (event) => {
+      if (event.key == " " && this.isOnGround) {
+        let acc = this.jumpForce / this.mass;
+        this.vel.y += acc * this.engine.deltaTime * this.jumpTime;
+      }
+
+      if(event.key == "d") this.dDown = true;
+      if(event.key == "a") this.aDown = true;
+    };
+
+    document.onkeyup = (event) =>{
+      if(event.key == "d") this.dDown = false;
+      if(event.key == "a") this.aDown = false;
     }
+  }
+
+  update() {
     this.pos.x += this.vel.x;
     this.pos.y += this.vel.y;
+    this.checkGround();
+    this.colisionHandler();
     this.addGravity();
-  };
+    this.move()
+  }
 
-  draw = () => {
+  move(){
+    if(this.dDown){
+      this.vel.x = 6 * Metric.m * this.engine.deltaTime * 10;
+    }else if(this.aDown){
+      this.vel.x = -6 * Metric.m * this.engine.deltaTime * 10;
+    }else{
+      this.vel.x = 0;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "black";
     ctx.fillRect(
       this.pos.x,
       this.engine.height - this.pos.y - this.size.y,
       this.size.x,
       this.size.y
     );
-  };
+  }
 
-  addGravity(){
+  addGravity() {
     this.vel.y += -Metric.g * Metric.m * engine.deltaTime;
   }
 
+  colisionHandler() {
+    if (this.pos.y < 0) {
+      this.vel.y = 0;
+      this.pos.y = 0;
+    }
+  }
+
+  checkGround() {
+    if (this.pos.y <= 0) {
+      this.isOnGround = true;
+    } else {
+      this.isOnGround = false;
+    }
+  }
+}
+
+class Tile extends Gameobject {
+  type = 0;
+
+  onInit() {
+    console.log(this.pos)
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+      this.pos.x,
+      this.engine.height - this.pos.y - this.size.y,
+      this.size.x,
+      this.size.y
+    );
+  }
+}
+
+class WorldGenerator extends Gameobject {
+  gridWidth = 0;
+  gridHeight = 0;
+
+  tiles = [[]];
+  tileSize = new Vector2D(50, 50);
+
+  update() {}
+
+  onInit() {
+    
+  }
 }
